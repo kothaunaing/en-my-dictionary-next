f0a23324f75bf576c88915b2b3ebc0b59a0a85bb
Than Aung Naing <aungnaingthan22@gmail.com>
2025-02-22 11:59:52 +0630

    fixed data fetching error

--- src/app/word/page.js
+++ src/app/word/page.js
@@ -13,35 +13,50 @@ export async function generateMetadata({ searchParams }) {
 const Word = async ({ searchParams }) => {
   const { query } = await searchParams;
 
-  const response = await fetch(
-    `https://enmydictionaryapi.onrender.com/word/${query}`
-  );
-
-  const results = await response.json();
-
-  // console.log(results);
-
-  if (results?.word) {
-    return (
-      <div className="m-2 space-y-4">
-        <div>
-          <div>
-            <h1 className="font-bold text-3xl">{results.word}</h1>
-          </div>
-          <div
-            className="break-words whitespace-pre-wrap leading-relaxed"
-            dangerouslySetInnerHTML={{ __html: results.definition }}
-          />
-        </div>
-      </div>
+  try {
+    const response = await fetch(
+      `https://enmydictionaryapi.onrender.com/word/${query}`
     );
-  } else {
+
+    const results = await response.json();
+
+    // console.log(results);
+
+    if (results?.word) {
+      return (
+        <div className="m-2 space-y-4">
+          <div>
+            <div>
+              <h1 className="font-bold text-3xl">{results.word}</h1>
+            </div>
+            <div
+              className="break-words whitespace-pre-wrap leading-relaxed"
+              dangerouslySetInnerHTML={{ __html: results.definition }}
+            />
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="h-[200px] flex items-center justify-center">
+          <div className="flex flex-col items-center gap-1">
+            <InfoIcon className="text-orange-500" />
+            <span className="font-semibold">
+              Word not found for <span className="italic">'{query}'</span>
+            </span>
+          </div>
+        </div>
+      );
+    }
+  } catch (error) {
+    console.log("Error fetching data..");
+
     return (
       <div className="h-[200px] flex items-center justify-center">
         <div className="flex flex-col items-center gap-1">
           <InfoIcon className="text-orange-500" />
           <span className="font-semibold">
-            Word not found for <span className="italic">'{query}'</span>
+            Server is restarting. Please wait a second and refresh
           </span>
         </div>
       </div>
